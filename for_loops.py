5d35f28a5ca389eb6956edf202f3f6c621b95733
kalmegati <kalmegati@gmail.com>
2020-09-23 15:52:04 -0400

    used loops to parse dicts

---
+++ for_loops.py
@@ -0,0 +1,4 @@
+example_dictionary = {'first_name': "Terrance", 'last_name': "KOAR", 'favorite_language': "Python"}
+for key, value in example_dictionary.items():
+    print("this is the key:", key)    
+    print("this is the value:", value, "\n")
