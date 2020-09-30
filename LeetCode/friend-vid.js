e2d4f0164b37344a87574b4e1edea5eaf1ff0a65
kalmegati <kalmegati@gmail.com>
2020-09-29 23:57:34 -0400

    created practice directory for code challenges

---
+++ LeetCode/friend-vid.js
@@ -0,0 +1,7 @@
+var watchedVideosByFriends = function(watchedVideos, friends, id, level) {
+    const bacon = friends[id] 
+    const vidTable = {}
+    console.log(bacon)
+};
+
+watchedVideosByFriends([["A","B"],["C"],["B","C"],["D"]], [[1,2],[0,3],[0,3],[1,2]], 0, 1)
