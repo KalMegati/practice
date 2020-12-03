23694b8506e1acd1aae9a2ca5c43e4407a90d509
kalmegati <kalmegati@gmail.com>
2020-12-02 20:13:13 -0500

    differentiated int vs double primitive type

--- Java/declaration.java
+++ Java/declaration.java
@@ -3,7 +3,7 @@ public class declaration {
     public static void main(String[] args) {
         int a = 10;
         int b = 5;
-        int c = a + b;
+        double c = a + b;
         System.out.println( a + " + " + b + " = " + c);   
     }
 
