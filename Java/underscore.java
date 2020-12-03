bbe99f1d35d2b044dd09325109f8dd5e05e0a83f
kalmegati <kalmegati@gmail.com>
2020-12-02 20:48:07 -0500

    underscore can be placed between digits for an int primitive

---
+++ Java/underscore.java
@@ -0,0 +1,17 @@
+public class underscore {
+
+
+    public static void main(String[] args) {
+        int x = 42;
+
+        int y = 4_3;
+
+        if (x == y) {
+            System.out.println("Success!");
+        }
+
+        if (x != y) {
+            System.out.println("Failure.");
+        }
+    }
+}
