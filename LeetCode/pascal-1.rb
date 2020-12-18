428bff15d07d7017733cae1970739381c3caa552
kalmegati <kalmegati@gmail.com>
2020-12-17 21:49:23 -0500

    cleaned testing breaks out of code

--- LeetCode/pascal-1.rb
+++ LeetCode/pascal-1.rb
@@ -3,15 +3,11 @@
 def generate(num_rows)
     pascal = [[1]]
     row = 0
-    # puts pascal
     while row < num_rows-1
-        # puts "ROW #{row}"
         template = pascal[row].map {|a|a}
         template.unshift(0).push(0)
         result = []
-        # puts template
         (template.length-1).times do |i|
-           
            result << template[i]+template[i+1]
         end
         pascal << result
