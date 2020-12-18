b46480c073d1a41e7ad9a1c9e8d3a0450d642356
kalmegati <kalmegati@gmail.com>
2020-12-17 21:48:18 -0500

    fulfilled deliverables

--- LeetCode/pascal-1.rb
+++ LeetCode/pascal-1.rb
@@ -1,10 +1,17 @@
+# require 'pry'
+
 def generate(num_rows)
     pascal = [[1]]
     row = 0
-    while row < num_rows
-        template = pascal[row].unshift(0).push(0)
+    # puts pascal
+    while row < num_rows-1
+        # puts "ROW #{row}"
+        template = pascal[row].map {|a|a}
+        template.unshift(0).push(0)
         result = []
-        template.length.times do |i|
+        # puts template
+        (template.length-1).times do |i|
+           
            result << template[i]+template[i+1]
         end
         pascal << result
@@ -13,3 +20,4 @@ def generate(num_rows)
     pascal
 end
 
+print(generate(3))
