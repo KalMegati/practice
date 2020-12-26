6dfe17d67c54260339a33f39c3285e47907d4057
kalmegati <kalmegati@gmail.com>
2020-12-26 12:00:17 -0500

    added #insertAtBeginning method to LinkedList instance

--- MoreJS/linkedList.js
+++ MoreJS/linkedList.js
@@ -16,3 +16,15 @@ class LinkedList{
         this.head = null;
     }
 }
+
+LinkedList.prototype.insertAtBeginning = function(data){
+    // A newNode object is created with property data and next = null
+        let newNode = new Node(data);
+    // The pointer next is assigned head pointer so that both pointers now point at the same node.
+        newNode.next = this.head;
+    // As we are inserting at the beginning the head pointer needs to now point at the newNode. 
+        
+        this.head = newNode;
+        return this.head;
+}
+
