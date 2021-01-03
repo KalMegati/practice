1e0fb5a24bff9d14b936cacaa2e953a41ff68316
kalmegati <kalmegati@gmail.com>
2021-01-02 20:39:13 -0500

    created method to delete last node

--- MoreJS/linkedList.js
+++ MoreJS/linkedList.js
@@ -88,3 +88,25 @@ LinkedList.prototype.deleteFirstNode = function(){
     this.head = this.head.next;
     return this.head;
 }
+
+LinkedList.prototype.deleteLastNode = function(){
+    if(!this.head){
+        return null;
+    }
+    // if only one node in the list
+    if(!this.head.next){
+        this.head = null;
+        return;
+    }
+   let previous = this.head;
+   let tail = this.head.next;
+   
+   while(tail.next !== null){
+       previous = tail;
+       tail = tail.next;
+   }
+   
+   previous.next = null;
+   return this.head;
+}
+
