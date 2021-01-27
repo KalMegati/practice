2203a46523a8d2633a89f1050b74a49ff79d767f
kalmegati <kalmegati@gmail.com>
2021-01-27 03:56:35 -0500

    App contains areas for Netflix and Twitch

--- ViewingParty/viewing_party_draft/src/App.js
+++ ViewingParty/viewing_party_draft/src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import NetflixScreen from './Screens/NetflixScreen.js'
+import TwitchScreen from './Screens/TwitchScreen.js'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       
-      
+      <NetflixScreen />
+      <TwitchScreen />
     
     </div>
   );
