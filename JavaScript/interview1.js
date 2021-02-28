89a6c2a8d276a020423c6b6f8d06d5c961f736ae
kalmegati <kalmegati@gmail.com>
2021-02-28 01:48:11 -0500

    solved

--- JavaScript/interview1.js
+++ JavaScript/interview1.js
@@ -1,3 +1,5 @@
+
+
 class Deferred {
 
     constructor(){
@@ -17,9 +19,22 @@ class Deferred {
 
 }
 
+//-----
+
 // usage example
 let d = new Deferred();
 d.then(function(v){ console.log("1", v); return "a"; });
 d.then(function(v){ console.log("2", v); return "b"; });
 d.then(function(v){ console.log("3", v); return "c"; });
 d.resolve("hello");
+
+/*
+expected console output...
+1 hello
+2 a
+3 b
+*/
+
+// note: feel free to add any constructor, fields and/or internal methods you need
+// there are multiple ways to solve this. then&resolve are just the minimum requirements
+// other note: you cannot use any Promise or async function to solve the problem
--- JavaScript/interview2.js
+++ JavaScript/interview2.js
@@ -1,35 +1,59 @@
 class Deferred {
 
     constructor(){
-        this.store = []
-        this.temp = "oops"
-    }
-    
-    retVal(){
-        return this.temp
+    	this.store = []
+    	this.temp = null
     }
 
-    then(func){
+    retValue() {
+    	return this.temp
+    }
+
+    then(func) {
         this.store.push(func);
     }
 
-    resolve(value){
+    resolve(value) {
         this.temp = value
-        this.store.forEach( function(func){
-            this.temp = func(this.temp)
-            //callback?
-        })
-    }
 
+        // this.store[2](this.store[1](this.store[0](this.temp)))
+
+        while (this.store.length > 0) {
+
+            
+            if (this.temp instanceof Deferred) {
+                this.temp.store.push(this.store.shift())
+            } else {
+                this.temp = this.store.shift()(this.temp)
+            }
+
+        }
+        
+    }
 }
+
 
 let d = new Deferred();
 d.then(res=>{
     console.log("1", res);
     let innerDef = new Deferred();
-    setTimeout(function(){ innerDef.resolve("a"); console.log("fire!")}, 1500);
+    setTimeout(function(){ innerDef.resolve("a"); }, 1500);
+    // innerDef.resolve("a")
     return innerDef;
 });
-d.then(function(v){ console.log("2", v); return "b"; });
+d.then(function(res){
+    console.log("2", res);
+    let innerDef = new Deferred();
+    setTimeout(function(){ innerDef.resolve("b"); }, 1500);
+    // innerDef.resolve("a")
+    return innerDef;});
 d.then(function(v){ console.log("3", v); return "c"; });
 d.resolve("hello");
+
+// *//*
+// expected console output...
+// 1 hello
+// (1.5s delay here)
+// 2 a
+// 3 b
+// */
