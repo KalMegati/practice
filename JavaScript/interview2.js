8d32d1155d8901cd57f2827ebfccc4f9dc7bf384
kalmegati <kalmegati@gmail.com>
2021-02-28 02:06:34 -0500

    tested for longer input chains

--- JavaScript/interview2.js
+++ JavaScript/interview2.js
@@ -20,7 +20,6 @@ class Deferred {
 
         while (this.store.length > 0) {
 
-            
             if (this.temp instanceof Deferred) {
                 this.temp.then(this.store.shift())
             } else {
@@ -41,13 +40,23 @@ d.then(res=>{
     // innerDef.resolve("a")
     return innerDef;
 });
-d.then(function(res){
-    console.log("2", res);
-    let innerDef = new Deferred();
-    setTimeout(function(){ innerDef.resolve("b"); }, 1500);
-    // innerDef.resolve("a")
-    return innerDef;});
+// d.then(function(res){
+//     console.log("2", res);
+//     let innerDef = new Deferred();
+//     setTimeout(function(){ innerDef.resolve("b"); }, 1500);
+//     // innerDef.resolve("a")
+//     return innerDef;});
+d.then(function(v){ console.log("2", v); return "b"; });
 d.then(function(v){ console.log("3", v); return "c"; });
+d.then(function(v){ console.log("4", v); return "d"; });
+d.then(function(res){
+    console.log("5", res);
+    let innerDef = new Deferred();
+    setTimeout(function(){ innerDef.resolve("e"); }, 1500);
+    return innerDef;});
+d.then(function(v){ console.log("6", v); return "f"; });
+d.then(function(v){ console.log("7", v); return "g"; });
+d.then(function(v){ console.log("8", v); return "h"; });
 d.resolve("hello");
 
 // *//*
@@ -56,4 +65,10 @@ d.resolve("hello");
 // (1.5s delay here)
 // 2 a
 // 3 b
+// 4 c
+// 5 d
+// (1.5s delay here)
+// 6 e
+// 7 f
+// 8 g
 // */
