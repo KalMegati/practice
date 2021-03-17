b5999ca070ca3fa069ea6545bda7bce19a1e3028
kalmegati <kalmegati@gmail.com>
2021-03-16 20:58:06 -0400

    non-main methods of classes can also take arguments

--- Java/HelloWorld.java
+++ Java/HelloWorld.java
@@ -1,9 +1,9 @@
 abstract class HelloWorld {
     public static void main(String[] args) {
-        System.out.println(intermediate()); 
+        System.out.println(intermediate(args)); 
     }
 
-    public static int intermediate() {
-        return 5;
+    public static int intermediate(String[] args) {
+        return (Integer.valueOf(args[0]));
     }
 }
